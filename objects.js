6a5e132ef3d654cfb1acd94c8aa384cadca8155f
Novera Lim <lim.novera88@gmail.com>
2018-02-10 18:31:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,7 @@ var playlist = { bruno: "finesse" };
 function updatePlaylist(playlist, bruno, thatsWhatILike) {
   return object.assign( {}, playlist, { [bruno]: thatsWhatILike });
 } 
-function removeFromPlaylist()
+function removeFromPlaylist(playlist, bruno) {
+  delete playlist.bruno;
+  return updatePlaylist;
+}
